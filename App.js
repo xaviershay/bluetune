4dde04d3d3678f4bc58808d3b5ed4e1a0f257d17
Xavier Shay <xavier.shay@ferocia.com.au>
2019-04-26 19:40:27 +1000

    WIP

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ const makeInterval = (name, semitones) => {
   };
 };
 
-// TODO: These intervals are wrong, just for testing.
 const INTERVALS = {
   "2m": makeInterval("Minor 2nd", 1),
   "2M": makeInterval("Major 2nd", 2),
@@ -79,6 +78,47 @@ const SCALE = [
   "b4"
 ];
 
+class PromisePlayer {
+  _player: Player;
+
+  constructor(filename) {
+    this._player = new Player(filename, { alwaysDestroy: false });
+  }
+
+  prepare() {
+    return new Promise((resolve, errorHandler) => {
+      this._player.prepare(err => {
+        if (err) {
+          errorHandler(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  play() {
+    return new Promise((resolve, _) => {
+      this._player.play(() => resolve());
+    });
+  }
+
+  stop() {
+    return new Promise((resolve, _) => {
+      this._player.stop(() => resolve());
+    });
+  }
+
+  destroy() {
+    return new Promise((resolve, _) => {
+      this._player.destroy(() => resolve());
+    });
+  }
+
+  state() {
+    return this._player.state;
+  }
+}
+
 export default class App extends React.PureComponent<Props, State> {
   state = {
     interval: "2m",
@@ -98,10 +138,6 @@ export default class App extends React.PureComponent<Props, State> {
 
   componentDidMount() {
     this.notes = {};
-    SCALE.forEach(note => {
-      this.notes[note] = new Player(note + ".mp3", { autoDestroy: false });
-      this.notes[note].prepare();
-    });
   }
 
   componentWillUnmount() {
@@ -109,13 +145,14 @@ export default class App extends React.PureComponent<Props, State> {
   }
 
   transitionGuess = () => {
+    const possibleIntervals = Object.keys(INTERVALS);
+    const n = Math.floor(Math.random() * possibleIntervals.length);
+
     // Generate a new test
-    const interval = INTERVALS[this.state.interval];
+    const interval = INTERVALS[possibleIntervals[n]];
     const baseIndex = Math.floor(
       Math.random() * (SCALE.length - interval.semitones)
     );
-    const possibleIntervals = Object.keys(INTERVALS);
-    const n = Math.floor(Math.random() * possibleIntervals.length);
 
     this.setState(
       {
@@ -132,27 +169,42 @@ export default class App extends React.PureComponent<Props, State> {
   };
 
   playInterval = () => {
-    Object.keys(this.notes).forEach(note => {
-      if (this.notes[note].canStop) {
-        this.notes[note].stop();
-      }
-    });
+    const stopPromises = Object.keys(this.notes)
+      .map(note => {
+        if (this.notes[note].canStop) {
+          return this.notes[note].stop();
+        }
+        return null;
+      })
+      .filter(Boolean);
 
     const interval = INTERVALS[this.state.interval];
 
     const baseNote = this.state.baseNote;
     const baseIndex = SCALE.indexOf(baseNote);
     const secondNote = SCALE[baseIndex + interval.semitones];
+    this.notes = {};
+    this.notes[baseNote] = new PromisePlayer(baseNote + ".mp3");
+    this.notes[secondNote] = new PromisePlayer(secondNote + ".mp3");
+    const preparePromises = [
+      this.notes[baseNote].prepare(),
+      this.notes[secondNote].prepare()
+    ];
 
-    setTimeout(() => {
-      this.notes[baseNote].play(() =>
-        setTimeout(() => this.notes[secondNote].play(), 1000)
-      );
-    }, 500);
+    // TODO: Handle starting new interval before last one finishes.
+    // TODO: this preparePromise doesn't really work, you can tell coz the
+    // setTimeout appears to take longer than it should.
+    Promise.all([...stopPromises, ...preparePromises])
+      .then(() => {
+        return this.notes[baseNote].play();
+      })
+      .then(() => setTimeout(() => this.notes[secondNote].play(), 500));
   };
 
   render() {
     switch (this.state.stage) {
+      case "loading":
+        return this.renderLoading();
       case "splash":
         return this.renderSplash();
       case "guess":
@@ -163,6 +215,16 @@ export default class App extends React.PureComponent<Props, State> {
         return <Text>Something went wrong :(</Text>;
     }
   }
+
+  renderLoading = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={[{ flex: 1, fontSize: 32 }, styles.instructions]}>
+          Loading...
+        </Text>
+      </View>
+    );
+  };
 
   renderSplash = () => {
     return (
